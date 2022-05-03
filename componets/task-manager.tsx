e44448df37079d51fff969bb597fbdf2916225c9
altay103 <buyukdemirgbd@gmail.com>
2022-05-04 05:01:50 +0600

    fix :  fix spawn system

--- componets/task-manager.tsx
+++ componets/task-manager.tsx
@@ -1,31 +1,52 @@
-import { VStack ,Flex,Button,Input } from "@chakra-ui/react";
+import { VStack, Flex, Button, Input } from "@chakra-ui/react";
 import { useState } from "react";
 import Task from "./task";
 
 
-function TaskManager(){
-    const [text,setText]=useState<string>("")
-    const [taskList,setTaskList]=useState([]);
-    const control=()=>{
-        if(text!=""){
-            setTaskList(taskList.concat(<Task taskName={text}/>))
-            
-            setText("")
-        }else{
-            alert("Input field can not be empty!")
-        }
-        
+export class TaskProps {
+    static Id = 0;
+    public taskId: number=0;
+    public taskName: string ="";
+    public taskEnabled: boolean = true;
+    constructor( name: string) {
+        this.taskName = name;
+        this.taskId = TaskProps.Id++;
     }
-    
-    
-    return(
-      
+}
+function TaskManager() {
+    const [text, setText] = useState<string>("")
+    const [taskList, setTaskList] = useState<TaskProps[]>([]);
+
+    function control() {
+        if (text != "") {
+            let task:TaskProps = new TaskProps(text);
+
+            setTaskList(taskList.concat(task));
+            setText("");
+        } else {
+            alert("Input field can not be empty!");
+        }
+
+    }
+
+
+
+   
+    return (
+
         <VStack>
             <Flex width='500px'>
-                <Input onChange={event=>setText(event.target.value)} value={text}/>
+                <Input onChange={event => setText(event.target.value)} value={text} />
                 <Button onClick={control} >Add</Button>
             </Flex>
-            {taskList}
+            {taskList.map((task) => {
+                return <Task key={task.taskId}
+                    taskName={task.taskName}
+                    taskEnabled={task.taskEnabled}
+                    taskId={task.taskId} 
+                    setTaskList={setTaskList} 
+                    taskList={taskList} />
+            })}
         </VStack>
 
     )
--- componets/task.tsx
+++ componets/task.tsx
@@ -1,41 +1,68 @@
-import { Button, Center, Checkbox, Flex, IconButton, Spacer, Text} from "@chakra-ui/react";
-import {AiOutlineClose} from "react-icons/ai";
-import { useState } from "react";
+import { Button, Center, Flex, IconButton, Text } from "@chakra-ui/react";
+import { AiOutlineClose } from "react-icons/ai";
+import { useCallback, useState } from "react";
+import TaskManager, { TaskProps } from "./task-manager";
 
-interface taskProps{
-    taskName:string;
+
+interface taskProps {
+    taskEnabled: boolean;
+    taskName: string;
+    taskId: number;
+    setTaskList: any;
+    taskList: any;
 }
-function Task(props:taskProps){
-    const [hide,setHide]=useState(true);
-    const [type,setType]=useState<any>("cite");
-    let changeType=()=>{
-        if(type=="del"){
-            setType("cite");
+function Task(props: taskProps) {
+    const [hide, setHide] = useState(true);
+    const [enabled,setEnabled] = useState(props.taskEnabled);
+    const changeType=()=>{
+        console.log(props.taskList.length);
+        let tempTaskList: TaskProps[] = props.taskList;
+        for (let i = 0; i < props.taskList.length; i++) {
+            if (tempTaskList[i].taskId == props.taskId) {
+                tempTaskList[i].taskEnabled = !tempTaskList[i].taskEnabled;
+                
+            }
+        }
+        
+        props.setTaskList(tempTaskList);
+        setEnabled(!enabled);
+        
+
+    }
+        
+    
+    function type(enabled:boolean){
+        //console.log(props.taskEnabled ? "enabled" : "disabled");
+        if(enabled){
+            return "cite";
         }else{
-            setType("del");   
+            return "del";
         }
     }
-    if(hide){
-        return(
-        <Center>
-            
-            <Flex w='500px' >
-                <Button flex={1} variant='ghost' onClick={changeType}>
-                    <Text as={type}>{props.taskName}</Text>          
-                </Button>
-            
-                <IconButton 
+    //console.log(props.taskName+" rendered");
+    if (hide) {
+        return (
+            <Center>
+
+                <Flex w='500px' >
+                    <Button flex={1} variant='ghost' onClick={changeType}>
+                        <Text as={type(enabled)}>{props.taskName}</Text>
+                    </Button>
+
+                    <IconButton
                         icon={<AiOutlineClose />}
-                        onClick={() => setHide(false)} 
-                        aria-label={""}>   
-                </IconButton>
-            </Flex>
-        
-         </Center>
+                        onClick={() => setHide(false)}
+                        aria-label={""}>
+                    </IconButton>
+                </Flex>
+
+            </Center>
         )
-    }else{
+    } else {
         return false
     }
 }
 
 export default Task;
+
+
