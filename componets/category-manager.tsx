1e83f59a9504b1299368871b76585117630566db
emre <yalcnnemre@gmail.com>
2022-05-13 00:49:06 +0300

    aaaaaaaaaa

--- componets/category-manager.tsx
+++ componets/category-manager.tsx
@@ -1,87 +1,94 @@
-import { Text, VStack, Flex, Button, Input, IconButton } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+  Text,
+  VStack,
+  Flex,
+  Button,
+  Input,
+  IconButton,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { MdAdd } from "react-icons/md";
 import Category from "./category";
 import { TodoProps } from "./todo";
 
-class CategoryProps {
-    static Id:number=0;
-    public categoryId:number=0;
-    public categoryName: string="";
-    public todos: TodoProps[]=[];
-    public categoryList:any;
-    public setCategoryList:any;
-    constructor(name:string){
-        this.categoryName=name;
-        this.categoryId=CategoryProps.Id++;
-    }
-}
+/* class CategoryProps {
+  static Id: number = 0;
+  public categoryId: number = 0;
+  public categoryName: string = "";
+  public todos: TodoProps[] = [];
+  public categoryList: any;
+  public setCategoryList: any;
+  constructor(name: string) {
+    this.categoryName = name;
+    this.categoryId = CategoryProps.Id++;
+  }
+} */
 
+interface CategoryInfo {
+  categoryName: string;
+  todos: Array<any>;
+}
 
 function CategoryManager(): JSX.Element {
-    const [text, setText] = useState<string>("");
-    const [categoryList, setCategoryList] = useState<CategoryProps[]>([]);
-    const [active, setActive] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
+  const [categoryList, setCategoryList] = useState<Array<CategoryInfo>>([]);
+  const [active, setActive] = useState<boolean>(false);
+  const [testState, setTest] = useState("");
+  function spawnCategory() {
+    text != "" &&
+      !categoryList.some((el) => el.categoryName === text) &&
+      setCategoryList(categoryList.concat({ categoryName: text, todos: [] }));
+    setText("");
+  }
+  function addButton() {
+    setActive(true);
+  }
+  function closeButton() {
+    setActive(false);
+  }
 
-
-    function spawnCategory() {
-        
-        if(text!=""){
-            setText("");
-            
-            let category:CategoryProps=new CategoryProps(text);
-            setCategoryList(categoryList.concat(category));
-            
-            
-
-        }else{
-            alert("Input field can not be empty!");
-        }
-    }
-    function addButton(){
-        setActive(true);
-    }
-    function closeButton(){
-        setActive(false);
-    }
-    return (
-        <VStack>
-            <Text>Categories</Text>
-            {!active?
-            <IconButton
-                    icon={<MdAdd size="25px" />}
-                    isRound={true}
-                    aria-label={""} 
-                    onClick={addButton}/>
-            :
-            <Flex width='500px' gap="1">
-                <Input 
-                 onChange={event => setText(event.target.value)} 
-                 value={text} />
-                <Button 
-                onClick={spawnCategory} >Add</Button>
-                 <IconButton
-                            icon={<AiOutlineClose />}
-                            onClick={closeButton}
-                            aria-label={""}>
-                 </IconButton>
-            </Flex>
-            }
-            {categoryList.map((category) =>{
-                 console.log("rendere");
-                return <Category key={category.categoryId}
-                categoryName={category.categoryName}
-                todos={category.todos}
-                categoryId={category.categoryId} 
-                categoryList={categoryList} 
-                setCategoryList={setCategoryList}></Category>
-            })}
-            
-            
-        </VStack>
-    )
+  useEffect(() => {
+    console.log("categoryList =>", categoryList);
+  }, [categoryList]);
+  useEffect(() => {
+    console.log("test state=> ", testState);
+  }, [testState]);
+  return (
+    <VStack>
+      <Text>Categories</Text>
+      {!active ? (
+        <IconButton
+          icon={<MdAdd size="25px" />}
+          isRound={true}
+          aria-label={""}
+          onClick={addButton}
+        />
+      ) : (
+        <Flex width="500px" gap="1">
+          <Input
+            onChange={(event) => setText(event.target.value)}
+            value={text}
+          />
+          <Button onClick={spawnCategory}>Add</Button>
+          <IconButton
+            icon={<AiOutlineClose />}
+            onClick={closeButton}
+            aria-label={""}
+          ></IconButton>
+        </Flex>
+      )}
+      {categoryList.map((category, index) => {
+        /*    console.log("rendere"); */
+        return (
+          <Category
+            key={String(index)}
+            values={{ category, categoryList, setCategoryList }}
+          ></Category>
+        );
+      })}
+    </VStack>
+  );
 }
 
-export default  CategoryManager;
-export type { CategoryProps };
+export default CategoryManager;
--- componets/category.tsx
+++ componets/category.tsx
@@ -1,107 +1,104 @@
-import { Box, Button, Center, Flex, IconButton, Input, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  IconButton,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useState } from "react";
-import { CategoryProps } from "./category-manager"
 import { MdAdd } from "react-icons/md";
 import { AiOutlineClose } from "react-icons/ai";
-import { BsTrash } from "react-icons/bs"
+import { BsTrash } from "react-icons/bs";
 import Todo, { TodoProps } from "./todo";
 
-function Category(props: CategoryProps): JSX.Element {
-    const [active, setActive] = useState(false);
-    const [text, setText] = useState("");
+function Category({
+  values: { category, categoryList, setCategoryList },
+}): JSX.Element {
+  /*   console.log("propss => ", props); */
+  const [active, setActive] = useState(false);
+  const [text, setText] = useState("");
 
-    function addButton() {
-        setActive(true);
-    }
-    function spawnTodo() {
-        if (text != "") {
-            setText("");
-            
-            for (let i = 0; i < props.categoryList.length; i++) {
-                if(props.categoryList[i].categoryId===props.categoryId){
-                    let tempCategoryList: CategoryProps[]=props.categoryList;
-                    tempCategoryList[i].todos=tempCategoryList[i].todos.concat(new TodoProps(text));
-                   
-                    props.setCategoryList(tempCategoryList);
-                    break;
-                }
-            }
-        } else {
-            alert("Input field can not be empty!");
-        }
-    }
-    function closeButton() {
-        setActive(false);
-    }
-    function deleteCategory() {
-        let tempCategoryList: CategoryProps[] = [];
-        for (let i = 0; i < props.categoryList.length; i++) {
-            if (props.categoryId !== props.categoryList[i].categoryId) {
-                tempCategoryList = tempCategoryList.concat(props.categoryList[i]);
-            }
-        }
-        props.setCategoryList(tempCategoryList);
-    }
-    return (
+  function addButton() {
+    setActive(true);
+  }
+  function spawnTodo() {
+    text !== ""
+      ? setCategoryList(
+          categoryList.map((el) =>
+            el.categoryName === category.categoryName
+              ? { ...el, todos: [...el.todos, new TodoProps(text)] }
+              : el
+          )
+        )
+      : alert("Input field can not be empty!");
+  }
+  function closeButton() {
+    setActive(false);
+  }
+  function deleteCategory() {
+    setCategoryList(
+      categoryList.filter((val) => val.categoryName !== category.categoryName)
+    );
+  }
+  return (
+    <VStack>
+      <Box width="500px" bgColor="gray.100">
+        <Flex width="500px" justify="center" gap="1">
+          <Center>
+            <Text isTruncated width="300px" align="center" fontSize="xl">
+              {category.categoryName}
+            </Text>
+          </Center>
+
+          {!active ? (
+            <>
+              <IconButton
+                icon={<MdAdd size="25px" />}
+                isRound={true}
+                aria-label={""}
+                onClick={addButton}
+              />
+              <IconButton
+                icon={<BsTrash size="25px" />}
+                isRound={true}
+                aria-label={""}
+                onClick={deleteCategory}
+              />
+            </>
+          ) : (
+            <>
+              <Input
+                width="200px"
+                placeholder="todo girrr"
+                onChange={(event) => setText(event.target.value)}
+                value={text}
+              />
+              <Button onClick={spawnTodo}>Add</Button>
+              <IconButton
+                icon={<AiOutlineClose />}
+                onClick={closeButton}
+                aria-label={""}
+              ></IconButton>
+            </>
+          )}
+        </Flex>
+      </Box>
+      <Box width="450px" bgColor="gray.100">
         <VStack>
-            <Box width="500px" bgColor="gray.100">
-                <Flex width="500px" justify="center" gap="1">
-                    <Center>
-                        <Text isTruncated
-                            width="300px"
-                            align="center"
-                            fontSize="xl">{props.categoryName}</Text>
-                    </Center>
-
-                    {!active ?
-                        <>
-                            <IconButton
-                                icon={<MdAdd size="25px" />}
-                                isRound={true}
-                                aria-label={""}
-                                onClick={addButton}
-                            />
-                            <IconButton
-                                icon={<BsTrash size="25px" />}
-                                isRound={true}
-                                aria-label={""}
-                                onClick={deleteCategory}
-                            />
-                        </>
-                        :
-                        <>
-                            <Input width="200px"
-                                onChange={event => setText(event.target.value)}
-                                value={text} />
-                            <Button
-                                onClick={spawnTodo}>Add</Button>
-                            <IconButton
-                                icon={<AiOutlineClose />}
-                                onClick={closeButton}
-                                aria-label={""}>
-                            </IconButton>
-                        </>
-                    }
-                </Flex>
-            </Box>
-            <Box width="450px" bgColor="gray.100">
-                <VStack>
-                    {props.todos.map((todo) =>{
-                        return(
-                            <Center>
-                                <Todo todoId={todo.todoId} todoName={todo.todoName} ></Todo>
-                            </Center>
-                        )   
-                    })}
-                </VStack>
-
-
-            </Box>
-
+          {category.todos.map((todo, index) => {
+            return (
+              <Center key={String(index)}>
+                <Todo todoId={todo.todoId} todoName={todo.todoName}></Todo>
+              </Center>
+            );
+          })}
         </VStack>
-    )
+      </Box>
+    </VStack>
+  );
 }
 
 export default Category;
-
-
